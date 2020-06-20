1d38de49bca35c7acb51a49b77d5208e4b57bb8d
osboxes.org <osboxes@dev.synkriom.com>
2020-06-19 22:32:28 -0700

    index.js routing fix

--- src/index.js
+++ src/index.js
@@ -6,14 +6,10 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import ScheduleInterviewForm from "./components/ScheduleInterviewForm";
 
-import reactLogo from "./assets/img/react-logo.svg";
 import ovLogo from "./assets/img/VeriKlick-Logo.png";
-import ovLogo2 from "./assets/img/VeriKlick-Logo.png";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-datepicker/dist/react-datepicker.css";
-import VideoRoom from "./components/VideoRoom";
 import VideoComponent from "./components/VideoComponent";
-import AssessmentComponent from "./components/assessment/AssessmentComponent";
 
 ReactDOM.render(
   <div>
